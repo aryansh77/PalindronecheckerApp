78a89cb14b2b4dfeff50fa770d0cdfabfb8362bb
aryansh77 <as2243@srmist.edu.in>
2026-02-26 15:43:46 +0530

    Aryan Added HardcodedPalindrome UC2

---
+++ App/src/PalindroneCheckerApp.java
@@ -0,0 +1,21 @@
+public class PalindroneCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Hardcoded string
+        String word = "madam";
+
+        // Reverse the string
+        String reverse = "";
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reverse = reverse + word.charAt(i);
+        }
+
+        // Check if palindrome
+        if (word.equals(reverse)) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is Not a Palindrome");
+        }
+    }
+}
