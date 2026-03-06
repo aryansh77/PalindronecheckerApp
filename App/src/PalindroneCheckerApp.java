e869ff6c8a4bfff40ba10e8ba910f456fd4120e7
Aryan <as2243@srmist.edu.in>
2026-03-06 14:54:27 +0530

    Added UC9 RecursivePalindrome

--- App/src/PalindroneCheckerApp.java
+++ App/src/PalindroneCheckerApp.java
@@ -1,78 +1,34 @@
 import java.util.Scanner;
-import java.util.Deque;
-import java.util.ArrayDeque;
 
-public class UseCase7PalindromeCheckerApp {
+public class PalindroneCheckerApp {
 
-    public static void main(String[] args) {
+    // Recursive function
+    public static boolean isPalindrome(String str, int start, int end) {
 
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a string: ");
-        String input = sc.nextLine();
-
-        Deque<Character> deque = new ArrayDeque<>();
-
-        // Insert characters into deque
-        for (int i = 0; i < input.length(); i++) {
-            deque.addLast(input.charAt(i));
+        // Base condition
+        if (start >= end) {
+            return true;
         }
 
-        boolean palindrome = true;
-
-        // Compare front and rear
-        while (deque.size() > 1) {
-
-            char first = deque.removeFirst();
-            char last = deque.removeLast();
-
-            if (first != last) {
-                palindrome = false;
-                break;
-            }
+        // If characters don't match
+        if (str.charAt(start) != str.charAt(end)) {
+            return false;
         }
 
-        if (palindrome) {
-            System.out.println("The string is a Palindrome.");
-        } else {
-            System.out.println("The string is NOT a Palindrome.");
-        }
-
-        sc.close();
+        // Recursive call
+        return isPalindrome(str, start + 1, end - 1);
     }
-}import java.util.Scanner;
-import java.util.Deque;
-import java.util.ArrayDeque;
-
-public class UseCase7PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Enter a string: ");
         String input = sc.nextLine();
 
-        Deque<Character> deque = new ArrayDeque<>();
+        boolean result = isPalindrome(input, 0, input.length() - 1);
 
-        // Insert characters into deque
-        for (int i = 0; i < input.length(); i++) {
-            deque.addLast(input.charAt(i));
-        }
-
-        boolean palindrome = true;
-
-        // Compare front and rear
-        while (deque.size() > 1) {
-
-            char first = deque.removeFirst();
-            char last = deque.removeLast();
-
-            if (first != last) {
-                palindrome = false;
-                break;
-            }
-        }
-
-        if (palindrome) {
+        if (result) {
             System.out.println("The string is a Palindrome.");
         } else {
             System.out.println("The string is NOT a Palindrome.");
